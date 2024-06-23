fa25581bca3ca2c09e450075d7a9339f55aedca6
FIRST_NAME LAST_NAME <shabazbazigar@gmail.com>
2024-06-23 17:56:54 +0530

    Deployment Pipeline

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-// Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+// MongoDB connection URI from environment variable
+const MONGODB_URI = process.env.MONGODB_URI
 
-app.use(express.static('dist'))
-
-app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB')
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error)
+  process.exit(1) // Exit the application if unable to connect to MongoDB
 })
 
+// Define MongoDB schema and models as needed
+// Example:
+// const User = mongoose.model('User', { name: String });
+
+// Serve static files from 'dist' directory
+app.use(express.static('dist'))
+
+// Example API endpoint
+app.get('/version', (req, res) => {
+  res.send('1') // Change this string to ensure a new version is deployed
+})
+
+// Start the Express server
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-
-
-  console.log('server started on port 5000')
+  console.log(`Server started on port ${PORT}`)
 })
