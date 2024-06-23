9334954dfb84b31a4043a2b47f36c6d86f2340b6
FIRST_NAME LAST_NAME <shabazbazigar@gmail.com>
2024-06-23 12:56:15 +0530

    Deployment Pipeline

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './styles.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
